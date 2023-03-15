ee66cf6dd7d7273217a3de60eec3632f419e8d2b
Benjamin Osborn <121897822+Osbjorn6352@users.noreply.github.com>
2023-03-14 23:03:25 -0500

    Update TwoSum.java
    Made a correction within the inner loop

--- TwoSum.java
+++ TwoSum.java
@@ -2,7 +2,7 @@ class TwoSum.java {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         for (int i = 0; i < nums.length; i++){
-            for (int j = i + 1; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
                 if (nums[i] + nums[j] == target) {
                     ans[0] = i;
                     ans[1] = j;
